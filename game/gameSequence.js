33f4d20d9e2e977176bc741434956d369785d068
aglindstrom <lindstromandrew@gmail.com>
2015-08-30 22:24:04 -0400

    Update gameSequence.js

--- game/gameSequence.js
+++ game/gameSequence.js
@@ -10,13 +10,13 @@ function loadassets(assets){
 
 	//assets to be loaded//
 	assets.push(new background("./images/testbg.png", 128, 72));
+	assets.push(new player("./images/char_block.png", 760, 268, 32, 32));
 	assets.push(new rigid("./images/riser_big.png", 300, 264, 22, 36, 8, true));
 	assets.push(new rigid("./images/riser_big.png", 1200, 264, 22, 36, 8, true));
 	assets.push(new rigid("./images/riser_big.png", 0, 132, 22, 36, 4, true));
 	assets.push(new rigid("./images/riser_big.png", 712, 132, 22, 36, 8, true));
 	assets.push(new rigid("./images/riser_big.png", 712, 396, 22, 36, 8, true));
 	assets.push(new rigid("./images/Bridge_1.png", 0, 500, 83, 36, true, false));
-	assets.push(new player("./images/char_block.png", 760, 268, 32, 32));
 }
 
 function update(assets){
