4a234eafaaade338f1750a97c4ee0224a468ddb8
Andrew <lindstromandrew@gmail.com>
2015-10-13 10:44:09 -0400

    working out sound bug

--- sites/andrewlindstrom/op1/script.js
+++ sites/andrewlindstrom/op1/script.js
@@ -1,7 +1,7 @@
 var prevsrcnum = 0;
 var elements = 0;
 
-function hovering(tag){
+function hovering(){
 	var srcnum = (Math.floor(Math.random()*4));
 	var sources = ["hoversound_a.mp3", "hoversound_c.mp3", "hoversound_d.mp3", "hoversound_e.mp3", "hoversound_g.mp3"];
 	
@@ -17,26 +17,46 @@ function hovering(tag){
 function createAudio(source)
 {
 	console.log("making");
-	var tag = String("audio_tag" + elements);
+	var tag = String("audio_tag_" + elements);
 	var audio = document.createElement("audio");
 	var parent = document.getElementById("audioWrap");
+
+
+	//placement//
 	parent.appendChild(audio);
 
+	//data//
 	audio.id = tag;
 	audio.src = source;
 	audio.volume = .25;
-	audio.addEventListener('ended',destroyAudio(tag), true);
+
+	//commands//
 	audio.load();
 	audio.play();
+
 	elements ++;
-	elements = elements%5;
+
+	//events//
+	addEvent(audio);
+
+	//logs//
+	console.log(elements);
+	console.log(parent);
+	console.log(audio);
+
 }
 
 function destroyAudio(source)
 {
 	console.log("removing");
-	var audio = document.getElementById(String(source));
-	var parent = audio.parentNode;
+	var parent = source.parentNode;
 
-	parent.removeChild(audio);
+	parent.removeChild(source);
+	elements --;
+	console.log(elements);
+}
+
+function addEvent(audio)
+{
+	audio.addEventListener('ended', function(){destroyAudio(audio)}, false);
 }
