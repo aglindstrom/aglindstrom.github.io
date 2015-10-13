834ec81f691308bf93133259078fb66b39bd2c3a
Andrew <lindstromandrew@gmail.com>
2015-10-12 21:34:28 -0400

    changes to sound

--- sites/andrewlindstrom/op1/script.js
+++ sites/andrewlindstrom/op1/script.js
@@ -1,20 +1,42 @@
 var prevsrcnum = 0;
+var elements = 0;
+
 function hovering(tag){
 	var srcnum = (Math.floor(Math.random()*4));
+	var sources = ["hoversound_a.mp3", "hoversound_c.mp3", "hoversound_d.mp3", "hoversound_e.mp3", "hoversound_g.mp3"];
+	
 	if(srcnum == prevsrcnum)
 	{
 		srcnum ++;
 	}
 	prevsrcnum = srcnum;
 
-	var sources = ["hoversound_a.mp3", "hoversound_c.mp3", "hoversound_d.mp3", "hoversound_e.mp3", "hoversound_g.mp3"];
-	var audio = document.getElementById(tag);
+	createAudio(sources[srcnum]);
+}
+
+function createAudio(source)
+{
+	console.log("making");
+	var tag = String("audio_tag" + elements);
+	var audio = document.createElement("audio");
+	var parent = document.getElementById("audioWrap");
+	parent.appendChild(audio);
+
+	audio.id = tag;
+	audio.src = source;
+	audio.volume = .25;
+	audio.addEventListener('ended',destroyAudio(tag), true);
 	audio.load();
 	audio.play();
+	elements ++;
+	elements = elements%5;
+}
 
-	var children = audio.childNodes;
-	children[1].src = sources[srcnum];
+function destroyAudio(source)
+{
+	console.log("removing");
+	var audio = document.getElementById(String(source));
+	var parent = audio.parentNode;
 
-	console.log(children);
-	console.log(String("playing" + tag + ", " + srcnum));
+	parent.removeChild(audio);
 }
