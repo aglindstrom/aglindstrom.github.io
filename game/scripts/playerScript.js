93e4d03961457f125ada10ef44869530e393a775
aglindstrom <lindstromandrew@gmail.com>
2016-04-02 09:58:44 -0400

    Update playerScript.js

--- game/scripts/playerScript.js
+++ game/scripts/playerScript.js
@@ -14,7 +14,7 @@ function player(location, x, y, width, height){
 	//variables//
 	this.speed = 0;
 	this.jump = 0;
-	this.jumpSpeed = .75;
+	this.jumpSpeed = 1.5;
 	this.grav = .06;
 	this.accel = .05;
 	this.deccel = .02;
