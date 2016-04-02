47cbdb088eee58a89805818b7c20c60d2893c62b
aglindstrom <lindstromandrew@gmail.com>
2016-04-02 13:16:22 -0400

    Update rigidGenerator.js

--- game/scripts/rigidGenerator.js
+++ game/scripts/rigidGenerator.js
@@ -2,3 +2,25 @@
 
 */
 
+function rigidGenerator(assets){
+  
+  this.hasGenerated = false;
+  
+  this.generate = function(){makeRigids(this, assets);};
+}
+
+function makeRigids(rigidGenerator, assets){
+  var x = 0;
+  var y = 0;
+  var width = 32;
+  var height = 32;
+  var spriteLoc = "./images/enemy.png";
+  var ammount = 5;
+  
+  if(!rigidGenerator.hasGenerated){
+    for(var i = 0; i < ammount; i++)
+    {
+      assets.push(new rigid(spriteLoc, x+(width*i), y, width, height));
+    }
+  }
+}
