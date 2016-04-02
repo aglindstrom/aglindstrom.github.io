297a7108d2ffb3832736286f025b608fd43fc4fa
aglindstrom <lindstromandrew@gmail.com>
2016-04-02 13:08:36 -0400

    Update rigidScript.js

--- game/scripts/rigidScript.js
+++ game/scripts/rigidScript.js
@@ -6,37 +6,9 @@
 function rigid(location, x, y, width, height){
 
 	this.sprite = new sprite(location, x, y, width, height);
-	this.hasBeenGenerated = false;
-	
-	this.generate = function(assets){rigidGenerate(this, assets);};
+
 	this.update = function (){};
 	this.render = function (){rigidRender(ctx, this.sprite);};
-	this.colided = function(){};
-}
-
-function rigidGenerate(t_rigid, assets)
-{
-	var x = 0;
-	var y = 0;
-	var width = 32;
-	var height = 32;
-	var location = "./images/enemy.png"
-	
-	rigidCalcXY(x,y);
-	
-	if(t_rigid.hasBeenGenerated == false){
-		for(var i = 0; i < 5; i++)
-		{
-			assets.push(new rigid(location, x+(width*i), y, width, height));
-		}
-		t_rigid.hasBeenGenerated = true;
-	}
-}
-
-function rigidCalcXY(x, y)
-{
-	x = window.width * Math.rand();
-	y = 500 * Math.rand();
 }
 
 function rigidRender(ctx, sprite)
